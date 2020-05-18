6ca630f9186b68b4a4be9c649de709e24bf09651
Tharuka Devendra <tsdevendra1@gmail.com>
2020-05-18 17:51:14 +0100

    added testing file

---
+++ testing.py
@@ -0,0 +1,23 @@
+class TreeNode:
+    def __init__(self, val):
+        self.left = None
+        self.right = None
+        self.val = val
+
+
+def x(N):
+    N -= 1
+    if N == 0: return [TreeNode(0)]
+    ret = []
+    for l in range(1, min(20, N), 2):
+        for left in x(l):
+            for right in x(N - l):
+                root = TreeNode(0)
+                root.left = left
+                root.right = right
+                ret += [root]
+    return ret
+
+
+if __name__ == "__main__":
+    x(3)
