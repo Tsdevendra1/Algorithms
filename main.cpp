4166f73db32d1afa9f89cfbe021db2a954321d4e
Tharuka Devendra <tsdevendra1@gmail.com>
2020-05-25 15:59:50 +0100

    changes

--- main.cpp
+++ main.cpp
@@ -6,55 +6,44 @@
 #include <queue>
 #include <unordered_set>
 #include <unordered_map>
+#include <string>
 #include <map>
 #include <set>
+#include <iostream>
+#include <string>
+
+#include <numeric>
+#include <sstream>
 
 using namespace std;
 
-void createFenwick(vector<int> &tree, const vector<int> &array) {
+class Solution {
+    vector<vector<int>> answer = {{}};
+    vector<int> possibleNumbers;
+    void dfs(vector<int> current, int currentIndex) {
+        if (currentIndex >= possibleNumbers.size()) {
+            return;
+        }
 
-    for (int i = 1; i < array.size() + 1; ++i) {
-        tree[i] = array[i - 1];
-    }
-
-    for (int i = 1; i < array.size() + 1; ++i) {
-        int lsb = i & -i;
-        int j = i + lsb;
-        if (j < array.size()+1) {
-            tree[j] += tree[i];
+        for (int i=currentIndex; i<possibleNumbers.size(); ++i) {
+            current.push_back(possibleNumbers[i]);
+            answer.push_back(current);
+            dfs(current, i+1);
+            current.pop_back();
         }
     }
-
-
-}
-
-int presum(vector<int> &tree, int index) {
-    int sum = 0;
-    int i = index + 1;
-    while (i != 0) {
-        sum += tree[i];
-        i -= (i & -i);
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> currentR;
+        possibleNumbers = nums;
+        dfs(currentR, 0);
+        return answer;
     }
-
-    return sum;
-
-}
-
-int presumRange(vector<int> &tree, int index1, int index2){
-    return presum(tree, index1) - presum(tree, index2-1);
-}
-
-bool randomi(vector<int> &arr){
-    short m[2001] = {}, s[1001] = {};
-    for (auto n : arr)
-        ++m[n + 1000];
-    for (auto i = 0; i < 2001; ++i)
-        if (m[i] && ++s[m[i]] > 1) return false;
-    return true;
-}
-
+};
 int main() {
-    vector<int> arr = {0, 1,2,2,3,3, 4,5};
-    randomi(arr);
-
+    char x = 'a';
+    bool r = 'a' < 9999;
+    min(3, 3)
+    cout << r << endl;
+return 0;
 }
