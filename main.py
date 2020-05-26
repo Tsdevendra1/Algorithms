33da4baf0b0c917ddd27f648a6f9503836e38a3c
Tharuka Devendra <tsdevendra1@gmail.com>
2020-05-26 08:30:25 +0100

    added some dp problem to main for testing

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
-class TreeNode:
-    def __init__(self, val):
-        self.left = None
-        self.right = None
-        self.val = val
+class Solution:
+    def longestDecomposition(self, text: str) -> int:
+        self.memo = {}
 
+        def dp(start, end):
+            pair = (start, end)
+            if pair in self.memo:
+                return self.memo[pair]
 
-def x(N):
-    N -= 1
-    if N == 0: return [TreeNode(0)]
-    ret = []
-    for l in range(1, min(20, N), 2):
-        for left in x(l):
-            for right in x(N - l):
-                root = TreeNode(0)
-                root.left = left
-                root.right = right
-                ret += [root]
-    return ret
+            if start == end:
+                return 1
+
+            score = 1
+            sub_string_distance = 0
+            while start + sub_string_distance < end - sub_string_distance:
+                if text[start: start + sub_string_distance+1] == text[end - sub_string_distance: end + 1]:
+                    print(text[start: start + sub_string_distance])
+                    new_start = start + sub_string_distance + 1
+                    new_end = end - sub_string_distance - 1
+                    score = max(score, 2 + dp(new_start, new_end))
+                sub_string_distance += 1
+
+            self.memo[pair] = score
+            return score
+
+        return dp(0, len(text) - 1)
 
 
 def main():
-    x(3)
+    x = Solution()
+    print(x.longestDecomposition("aaa"))
 
 
 if __name__ == "__main__":
