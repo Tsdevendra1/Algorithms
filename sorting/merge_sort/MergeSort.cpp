9bb86ead8c78ad8bf2d8788139538eea1c8a242a
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-01 18:30:36 +0100

    added placeholder for merge sorting

---
+++ sorting/merge_sort/MergeSort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tharuka Devendra on 01/06/2020.
+//
+
+#include "MergeSort.h"
---
+++ sorting/merge_sort/MergeSort.h
@@ -0,0 +1,14 @@
+//
+// Created by Tharuka Devendra on 01/06/2020.
+//
+
+#ifndef ALGORITHMS_MERGESORT_H
+#define ALGORITHMS_MERGESORT_H
+
+
+class MergeSort {
+
+};
+
+
+#endif //ALGORITHMS_MERGESORT_H
