7de5e6d9bb4ac2767a9807a42f2d9907e05db8ac
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-03 10:32:04 +0100

    pancake sort

---
+++ sorting/pancake_sort/PancakeSort.cpp
@@ -0,0 +1,55 @@
+//
+// Created by Tharuka Devendra on 03/06/2020.
+//
+
+#include "PancakeSort.h"
+#include <string>
+#include <vector>
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <map>
+#include <set>
+#include <iostream>
+#include <string>
+#include <stack>
+#include <numeric>
+#include <sstream>
+
+using namespace std;
+
+
+void flipVector(vector<int> &arr, int upTo){
+    int start = 0;
+    int end = upTo;
+    int temp;
+    while (start < end){
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        ++start;
+        --end;
+    }
+}
+
+void pancakeSort(vector<int> &arr) {
+    for (int size = arr.size(); size > 1; --size) {
+       int positionOfMax = max_element(arr.begin(), arr.begin()+size) - arr.begin();
+       int lastIndexOfCurrentSize = size-1;
+       if (positionOfMax != lastIndexOfCurrentSize) {
+           flipVector(arr, positionOfMax);
+           flipVector(arr, lastIndexOfCurrentSize);
+       }
+    }
+}
+
+void testPancakeSort() {
+    vector<int> test = {3,1,6,2,12};
+    pancakeSort(test);
+    for (auto i: test) {
+        cout << i << endl;
+    }
+}
---
+++ sorting/pancake_sort/PancakeSort.h
@@ -0,0 +1,15 @@
+//
+// Created by Tharuka Devendra on 03/06/2020.
+//
+
+#ifndef ALGORITHMS_PANCAKESORT_H
+#define ALGORITHMS_PANCAKESORT_H
+
+
+class PancakeSort {
+
+};
+
+void testPancakeSort();
+
+#endif //ALGORITHMS_PANCAKESORT_H
