5414503e0bae5d554032c0a9789be59342832cb8
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-05 17:16:52 +0100

    selection sort

---
+++ sorting/selection_sort/SelectionSort.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Tharuka Devendra on 05/06/2020.
+//
+
+#include "SelectionSort.h"
+#include "../../Utils.h"
+
+#include "vector"
+
+using namespace std;
+
+void selectionSort(vector<int> &arr) {
+    for (int endPosition = (int) arr.size() - 1; endPosition >= 0; ++endPosition) {
+        int positionOfMax = 0;
+        for (int i = 0; i < endPosition; ++i) {
+            if (arr[i] > arr[positionOfMax]) {
+                positionOfMax = i;
+            }
+        }
+        int temp = arr[positionOfMax];
+        arr[positionOfMax] = arr[endPosition];
+        arr[endPosition] = temp;
+
+    }
+}
+
+void testSelectionSort() {
+    vector<int> test = {1, 10, 5, 11, 9, 3, 7};
+    selectionSort(test);
+    Utils::printArr(test);
+
+}
---
+++ sorting/selection_sort/SelectionSort.h
@@ -0,0 +1,15 @@
+//
+// Created by Tharuka Devendra on 05/06/2020.
+//
+
+#ifndef ALGORITHMS_SELECTIONSORT_H
+#define ALGORITHMS_SELECTIONSORT_H
+
+
+class SelectionSort {
+
+};
+
+void testSelectionSort();
+
+#endif //ALGORITHMS_SELECTIONSORT_H
