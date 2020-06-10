161e5f170e92aa129aff57c37ad79ccedd1f1416
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-10 11:30:02 +0100

    setup

---
+++ bit_manipulation/BitwiseOperations.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tharuka Devendra on 10/06/2020.
+//
+
+#include "BitwiseOperations.h"
---
+++ bit_manipulation/BitwiseOperations.h
@@ -0,0 +1,17 @@
+//
+// Created by Tharuka Devendra on 10/06/2020.
+//
+
+#ifndef ALGORITHMS_BITWISEOPERATIONS_H
+#define ALGORITHMS_BITWISEOPERATIONS_H
+
+
+class BitwiseOperations {
+
+};
+
+void testBitwiseOperations(){
+
+}
+
+#endif //ALGORITHMS_BITWISEOPERATIONS_H
