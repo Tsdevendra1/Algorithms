89c0f33ee40e115627f086168038eff52a178db1
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-13 18:43:47 +0100

    added ability to check if cycle in top sort

--- graphs/toplogical_sort/TopologicalSort.cpp
+++ graphs/toplogical_sort/TopologicalSort.cpp
@@ -10,15 +10,24 @@
 #include <numeric>
 
 using namespace std;
+enum Colours {
+    white, gray, black
+};
 
-int dfs(int currentOrderIndex, int currentNode, Graph &graph, vector<int> &ordering, vector<bool> &seen) {
+int dfs(int currentOrderIndex, int currentNode, Graph &graph, vector<int> &ordering, vector<bool> &seen,
+        vector<Colours> &colours, bool &hasCycle) {
     seen[currentNode] = true;
     unordered_map<int, int> nodes = graph.neighboursForVertex(currentNode);
+    colours[currentNode] = gray;
     for (auto &[node, cost]: nodes) {
+        if (colours[node] == gray) {
+            hasCycle = true;
+        }
         if (!seen[node]) {
-            currentOrderIndex = dfs(currentOrderIndex, node, graph, ordering, seen);
+            currentOrderIndex = dfs(currentOrderIndex, node, graph, ordering, seen, colours, hasCycle);
         }
     }
+    colours[currentNode] = black;
     ordering[currentOrderIndex] = currentNode;
     return currentOrderIndex - 1;
 
@@ -28,12 +37,17 @@ vector<int> topologicalSort(Graph &graph) {
     int numNodes = graph.numberOfNodes();
     vector<bool> seen(numNodes, false);
     vector<int> ordering(numNodes);
+    vector<Colours> colours(numNodes);
     int indexForOrdering = numNodes - 1;
+    bool hasCycle = false;
 
     for (int atNode = 0; atNode < numNodes; ++atNode) {
         if (!seen[atNode]) {
-            indexForOrdering = dfs(indexForOrdering, atNode, graph, ordering, seen);
+            indexForOrdering = dfs(indexForOrdering, atNode, graph, ordering, seen, colours, hasCycle);
         }
+    }
+    if (hasCycle) {
+        throw logic_error("Can't topsort - found cycle");
     }
     return ordering;
 }
