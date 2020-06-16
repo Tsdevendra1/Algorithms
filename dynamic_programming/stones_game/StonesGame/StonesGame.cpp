daa49d80b7f0646b5ae20f3b99dab80ec42db5c0
Tharuka Devendra <tsdevendra1@gmail.com>
2020-06-16 17:23:29 +0100

    stone game implementation

---
+++ dynamic_programming/stones_game/StonesGame/StonesGame.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Tharuka Devendra on 16/06/2020.
+//
+
+#include <iostream>
+#include "StonesGame.h"
+#include "vector"
+#include "numeric"
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> stones;
+    vector<vector<int>> dp;
+
+
+    int memo(int start, int end, int isFirstPerson) {
+        if (start > end){
+            return 0;
+        }
+
+
+        int frontMove = memo(start+1, end, !isFirstPerson);
+        int add = (isFirstPerson) ? stones[start] : 0;
+        int frontMoveScore = frontMove + add;
+
+
+        int backMove = memo(start, end-1, !isFirstPerson);
+        int add2 = (isFirstPerson) ? stones[end] : 0;
+        int backMoveScore = backMove + add2;
+
+        if (isFirstPerson) {
+            return max(frontMoveScore, backMoveScore);
+        }
+        return min(frontMoveScore, backMoveScore);
+
+    }
+
+    bool canWinStoneGame(vector<int> &stonesOriginal) {
+        stones = stonesOriginal;
+        dp.resize(stonesOriginal.size(), vector<int>(stonesOriginal.size(), -1));
+        int totalSum = accumulate(stonesOriginal.begin(), stonesOriginal.end(), 0);
+        int secondPlayerScore = memo(0, (int) stonesOriginal.size() - 1, false);
+        cout << secondPlayerScore << endl;
+        cout << totalSum - secondPlayerScore << endl;
+        return secondPlayerScore < (totalSum - secondPlayerScore);
+
+
+    }
+};
+
+
+void testStoneGame(){
+    auto solution = make_unique<Solution>();
+    vector<int> stones = {5,3,4,5,3};
+    cout << solution->canWinStoneGame(stones)<<endl;
+}
---
+++ dynamic_programming/stones_game/StonesGame/StonesGame.h
@@ -0,0 +1,16 @@
+//
+// Created by Tharuka Devendra on 16/06/2020.
+//
+
+#ifndef ALGORITHMS_STONESGAME_H
+#define ALGORITHMS_STONESGAME_H
+
+
+class StonesGame {
+
+};
+
+void testStoneGame();
+
+
+#endif //ALGORITHMS_STONESGAME_H
