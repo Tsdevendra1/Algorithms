55f0b32a3c93153124e4acc67509d39a904bdc64
Tharuka Devendra <tsdevendra1@gmail.com>
2020-07-31 12:25:15 +0100

    added longest common substring bottom up approach

--- strings/longest_common_sub/LongestCommonSub.cpp
+++ strings/longest_common_sub/LongestCommonSub.cpp
@@ -17,7 +17,7 @@ public:
     LongestCommonSub(string &txt1, string &txt2) : txt1(txt1), txt2(txt2),
                                                    dp(txt1.size(), vector<int>(txt2.size(), 0)) {}
 
-    int solve(int i, int j) {
+    int topDown(int i, int j) {
         if (i >= txt1.size() || j >= txt2.size()) {
             return 0;
         }
@@ -28,16 +28,33 @@ public:
 
         int score;
         if (txt1[i] == txt2[j]) {
-            int positionOne = solve(i + 1, j + 1);
-            score = positionOne + 1;
+            int move = topDown(i + 1, j + 1);
+            score = move + 1;
         } else {
-            int positionOne = solve(i + 1, j);
-            int positionTwo = solve(i, j + 1);
-            score = max(positionOne, positionTwo);
+            int moveOne = topDown(i + 1, j);
+            int moveTwo = topDown(i, j + 1);
+            score = max(moveOne, moveTwo);
         }
 
         dp[i][j] = score;
         return score;
+    }
+
+    int bottomUp() {
+        dp.resize(txt1.size()+1, vector<int>(txt2.size()+1, 0));
+
+        for (int i = 1; i <= txt1.size(); ++i) {
+            int score = 0;
+            for (int j = 1; j <= txt2.size(); ++j) {
+                if (txt1[i-1] == txt2[j-1]) {
+                    score = dp[i-1][j-1] + 1;
+                } else {
+                    score = max(dp[i-1][j], dp[i][j-1]);
+                }
+                dp[i][j] = score;
+            }
+        }
+        return dp.back().back();
     }
 };
 
@@ -46,5 +63,7 @@ void testLongestCommonSub() {
     string txt1 = "AGGTAB";
     string txt2 = "GXTXAYB";
     auto solution = make_unique<LongestCommonSub>(txt1, txt2);
-    assert(solution->solve(0, 0) == 4);
+    assert(solution->topDown(0, 0) == 4);
+    auto solution1 = make_unique<LongestCommonSub>(txt1, txt2);
+    assert(solution1->bottomUp() == 4);
 }
