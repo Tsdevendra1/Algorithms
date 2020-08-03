4ba8281f4d8dd9287448d26a87df3d0461d96651
Tharuka Devendra <tsdevendra1@gmail.com>
2020-08-03 11:07:27 +0100

    added notepad page to write random stuff

---
+++ notepad.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Tharuka Devendra on 03/08/2020.
+//
+
+#include "notepad.h"
+#include "iostream"
+using namespace std;
+
+void notepad(){
+    cout << "hi";
+}
---
+++ notepad.h
@@ -0,0 +1,13 @@
+//
+// Created by Tharuka Devendra on 03/08/2020.
+//
+
+#ifndef ALGORITHMS_NOTEPAD_H
+#define ALGORITHMS_NOTEPAD_H
+
+
+
+void notepad();
+
+
+#endif //ALGORITHMS_NOTEPAD_H
