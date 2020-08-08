4972a10c9d703435076e569c1b7cd19348e38c34
Tharuka Devendra <tsdevendra1@gmail.com>
2020-08-08 20:05:34 +0100

    added space efficient bfs for permutations

--- dfs_bfs/generate_permutations/GeneratePermutations.cpp
+++ dfs_bfs/generate_permutations/GeneratePermutations.cpp
@@ -10,6 +10,45 @@
 
 using namespace std;
 
+class Permutations {
+public:
+    static vector<vector<int>> findPermutations(const vector<int> &nums) {
+
+        vector<vector<int>> result;
+
+        queue<vector<int>> permutations;
+
+        permutations.push(vector<int>());
+
+        for (auto currentNumber : nums) {
+            // we will take all existing permutations and add the current number to create new
+            // permutations
+            int n = permutations.size();
+
+            for (int i = 0; i < n; i++) {
+                vector<int> oldPermutation = permutations.front();
+                permutations.pop();
+                // create a new permutation by adding the current number at every position
+                for (int j = 0; j <= oldPermutation.size(); j++) {
+
+                    vector<int> newPermutation(oldPermutation);
+                    newPermutation.insert(newPermutation.begin() + j, currentNumber);
+
+                    if (newPermutation.size() == nums.size()) {
+                        result.push_back(newPermutation);
+                    } else {
+                        permutations.push(newPermutation);
+                    }
+
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
 vector<vector<int>> generateAllPermutations(vector<int> &choices) {
     vector<int> possibleChoices = choices;
     queue<pair<unordered_set<int>, vector<int>>> tracker;
@@ -43,5 +82,18 @@ vector<vector<int>> generateAllPermutations(vector<int> &choices) {
 void testGeneratePermutations() {
     vector<int> choices = {1, 2, 3};
     auto answer = generateAllPermutations(choices);
+    auto perms = make_unique<Permutations>();
+    auto answer2 = perms->findPermutations(choices);
+
+    int similar = 0;
+    for (auto &a1: answer){
+        for (auto &a2: answer2) {
+            if (a1 == a2){
+                ++similar;
+            }
+        }
+    }
+    assert(similar == 6);
+    assert(perms->findPermutations(choices).size() == 6);
     assert(answer.size() == 6);
 }
