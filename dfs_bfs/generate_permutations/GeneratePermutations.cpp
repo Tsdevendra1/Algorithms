9cde9fd30d1a7ed31a1f67fa962d700ffe2a6c54
Tharuka Devendra <tsdevendra1@gmail.com>
2020-08-14 10:38:18 +0100

    added another permutation algorithm

--- dfs_bfs/generate_permutations/GeneratePermutations.cpp
+++ dfs_bfs/generate_permutations/GeneratePermutations.cpp
@@ -7,49 +7,48 @@
 #include "vector"
 #include "unordered_set"
 #include "queue"
+#include "../../Utils.h"
 
 using namespace std;
 
-class Permutations {
-public:
-    static vector<vector<int>> findPermutations(const vector<int> &nums) {
+vector<vector<int>> perms1(const vector<int> &nums) {
 
-        vector<vector<int>> result;
+    vector<vector<int>> result;
 
-        queue<vector<int>> permutations;
+    queue<vector<int>> permutations;
 
-        permutations.push(vector<int>());
+    permutations.push(vector<int>());
 
-        for (auto currentNumber : nums) {
-            // we will take all existing permutations and add the current number to create new
-            // permutations
-            int n = permutations.size();
+    for (auto currentNumber : nums) {
+        // we will take all existing permutations and add the current number to create new
+        // permutations
+        int n = permutations.size();
 
-            for (int i = 0; i < n; i++) {
-                vector<int> oldPermutation = permutations.front();
-                permutations.pop();
-                // create a new permutation by adding the current number at every position
-                for (int j = 0; j <= oldPermutation.size(); j++) {
+        for (int i = 0; i < n; i++) {
+            vector<int> oldPermutation = permutations.front();
+            permutations.pop();
+            // create a new permutation by adding the current number at every position
+            for (int j = 0; j <= oldPermutation.size(); j++) {
 
-                    vector<int> newPermutation(oldPermutation);
-                    newPermutation.insert(newPermutation.begin() + j, currentNumber);
+                vector<int> newPermutation(oldPermutation);
+                newPermutation.insert(newPermutation.begin() + j, currentNumber);
 
-                    if (newPermutation.size() == nums.size()) {
-                        result.push_back(newPermutation);
-                    } else {
-                        permutations.push(newPermutation);
-                    }
-
+                if (newPermutation.size() == nums.size()) {
+                    result.push_back(newPermutation);
+                } else {
+                    permutations.push(newPermutation);
                 }
+
             }
         }
-
-        return result;
     }
-};
+
+    return result;
+}
 
 
-vector<vector<int>> generateAllPermutations(vector<int> &choices) {
+vector<vector<int>> perms2(vector<int> &choices) {
+    // Theres also the DFS version of this
     vector<int> possibleChoices = choices;
     queue<pair<unordered_set<int>, vector<int>>> tracker;
     tracker.push({});
@@ -79,11 +78,32 @@ vector<vector<int>> generateAllPermutations(vector<int> &choices) {
     return answer;
 }
 
+
+void perms3Helper(vector<int> &choices, int swapPosition, vector<vector<int>> &answer) {
+    // Base case
+    if (swapPosition == choices.size() - 1) {
+        answer.push_back(choices);
+    } else {
+        // Permutations made
+        for (int i = swapPosition; i < choices.size(); i++) {
+            swap(choices[swapPosition], choices[i]);
+            perms3Helper(choices, swapPosition + 1, answer);
+            swap(choices[swapPosition], choices[i]);
+        }
+    }
+}
+
+vector<vector<int>> perms3(vector<int> &choices) {
+    vector<vector<int>> answer;
+    perms3Helper(choices, 0, answer);
+    return answer;
+}
+
 void testGeneratePermutations() {
-    vector<int> choices = {1, 2, 3};
-    auto answer = generateAllPermutations(choices);
-    auto perms = make_unique<Permutations>();
-    auto answer2 = perms->findPermutations(choices);
+    vector<int> choices = {1, 2, 2};
+    auto answer = perms2(choices);
+    auto answer2 = perms1(choices);
+    auto answer3 = perms3(choices);
 
     int similar = 0;
     for (auto &a1: answer) {
@@ -94,6 +114,7 @@ void testGeneratePermutations() {
         }
     }
     assert(similar == 6);
-    assert(perms->findPermutations(choices).size() == 6);
+    assert(perms1(choices).size() == 6);
     assert(answer.size() == 6);
+    assert(answer3.size() == 6);
 }
