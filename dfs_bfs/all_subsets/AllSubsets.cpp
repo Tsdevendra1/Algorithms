1c89cc35485eed56a30fe597c2eb054988f92b6d
Tharuka Devendra <tsdevendra1@gmail.com>
2020-08-19 13:54:47 +0100

    added another backtrack algorithm for getting all subsets

--- dfs_bfs/all_subsets/AllSubsets.cpp
+++ dfs_bfs/all_subsets/AllSubsets.cpp
@@ -16,23 +16,48 @@ int numberOfSubsets(vector<int> &choices) {
     return pow(2, n);
 }
 
-void backtrack(vector<int> &current, int currentPosition, vector<int> &choices, vector<vector<int>> &answers){
-    if (currentPosition >= choices.size()){
+void backtrack(vector<int> &current, int currentPosition, vector<int> &choices, vector<vector<int>> &answers) {
+    if (currentPosition >= choices.size()) {
         answers.push_back(current);
         return;
     }
+
+    // excluding current element
     backtrack(current, currentPosition + 1, choices, answers);
     int valueForPosition = choices[currentPosition];
+
+    // adding current element
     current.push_back(valueForPosition);
     backtrack(current, currentPosition + 1, choices, answers);
     current.pop_back();
 
 }
 
-vector<vector<int>> getAllSubsetsBacktrack(vector<int> &choices){
+vector<vector<int>> getAllSubsetsBacktrack(vector<int> &choices) {
     vector<vector<int>> answer;
     vector<int> random = {};
-    backtrack(random, 0,choices, answer);
+    backtrack(random, 0, choices, answer);
+    return answer;
+}
+
+
+void backtrack2(vector<int> &currentSet, int currentPosition, vector<int> &choices, vector<vector<int>> &answer) {
+    // this method builds up by size (first iteration is size 1, then size 2, etc....
+    // by for looping we can include and skip each position for the corresponding subsequent sizes
+    answer.push_back(currentSet);
+
+    for (int position = currentPosition; position < choices.size(); ++position) {
+        int value = choices[position];
+        currentSet.push_back(value);
+        backtrack2(currentSet, position + 1, choices, answer);
+        currentSet.pop_back();
+    }
+}
+
+vector<vector<int>> getAllSubsetsBacktrack2(vector<int> &choices) {
+    vector<vector<int>> answer = {};
+    vector<int> random = {};
+    backtrack2(random, 0, choices, answer);
     return answer;
 }
 
@@ -64,8 +89,12 @@ void testAllSubsets() {
     assert(numberOfSubsets(test) == 8);
     auto subsets = getAllSubsets(test);
     auto subsetsBacktrack = getAllSubsetsBacktrack(test);
+    auto subsetsBacktrack2 = getAllSubsetsBacktrack2(test);
+
     assert(subsets.size() == 8);
     assert(subsetsBacktrack.size() == 8);
+    assert(subsetsBacktrack2.size() == 8);
     assert(Utils::compareVectors(subsets, subsetsBacktrack));
+    assert(Utils::compareVectors(subsetsBacktrack2, subsetsBacktrack));
 
 }
