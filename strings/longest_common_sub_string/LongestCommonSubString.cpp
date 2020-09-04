8d5d8cf9a91b1875e7b0a24f00a83fa06253b5d7
Tharuka Devendra <tsdevendra1@gmail.com>
2020-09-04 19:01:37 +0100

    added the accepted version of topdown longest common substring on leetcode

--- strings/longest_common_sub_string/LongestCommonSubString.cpp
+++ strings/longest_common_sub_string/LongestCommonSubString.cpp
@@ -10,6 +10,40 @@
 
 using namespace std;
 
+class TopdownLongestCommonSubstring {
+    vector<int> A;
+    vector<int> B;
+    vector<vector<int>> dp;
+    int answer = 0;
+public:
+    int topdownLongestCommonSubstring(vector<int> &C, vector<int> &D) {
+        dp.resize(C.size(), vector<int>(D.size(), -1));
+        A = C;
+        B = D;
+        memo(A.size() - 1, B.size() - 1);
+        return answer;
+    }
+
+    int memo(int one, int two) {
+        if (one < 0 || two < 0)
+            return 0;
+
+        if (dp[one][two] != -1) {
+            return dp[one][two];
+        }
+
+        int currentBest = 0;
+        if (A[one] == B[two]) {
+            currentBest = 1 + memo(one - 1, two - 1);
+        }
+        dp[one][two] = currentBest;
+        memo(one - 1, two);
+        memo(one, two - 1);
+        answer = max(answer, dp[one][two]);
+        return dp[one][two];
+    }
+};
+
 class LongestCommonSubString {
     string txt1;
     string txt2;
@@ -19,6 +53,7 @@ public:
                                                          dp(txt1.size(), vector<unordered_map<int, int>>(txt2.size())) {
     }
 
+    // this version doesn't actually work on leetcode, it timesout
     int topDown(int i, int j, int currentCount) {
         if (i >= txt1.size() || j >= txt2.size()) {
             return currentCount;
@@ -62,10 +97,13 @@ public:
 void testLongestCommonSubString() {
     string txt1 = "zxabcdezy";
     string txt2 = "yzabcdezx";
+    vector<int> test1 = {1, 2, 3, 4, 5, 6};
+    vector<int> test2 = {2, 1, 4, 3, 4, 5, 6};
 
     auto solution1 = make_unique<LongestCommonSubString>(txt1, txt2);
     auto solution2 = make_unique<LongestCommonSubString>(txt1, txt2);
-
+    auto solution3 = make_unique<TopdownLongestCommonSubstring>();
+    assert(solution3->topdownLongestCommonSubstring(test1, test2) == 4);
     assert(solution1->topDown(0, 0, 0) == 6);
     assert(solution2->bottomUp() == 6);
 }
