7cd84a30fcd8ec06d2c6893c240722f20eccde92
Tharuka Devendra <tsdevendra1@gmail.com>
2020-09-10 14:17:55 +0100

    moved files around to clarify which type of dp is each problem is

---
+++ dynamic_programming/merging_intervals/min_cost_tree_from_leafs/MinCostTreeFromLeafs.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tharuka Devendra on 10/09/2020.
+//
+
+#include "MinCostTreeFromLeafs.h"
+
+void testMinCostTreeFromLeafs() {
+
+};
---
+++ dynamic_programming/merging_intervals/min_cost_tree_from_leafs/MinCostTreeFromLeafs.h
@@ -0,0 +1,16 @@
+//
+// Created by Tharuka Devendra on 10/09/2020.
+//
+
+#ifndef ALGORITHMS_MINCOSTTREEFROMLEAFS_H
+#define ALGORITHMS_MINCOSTTREEFROMLEAFS_H
+
+
+class MinCostTreeFromLeafs {
+
+};
+
+
+void testMinCostTreeFromLeafs();
+
+#endif //ALGORITHMS_MINCOSTTREEFROMLEAFS_H
