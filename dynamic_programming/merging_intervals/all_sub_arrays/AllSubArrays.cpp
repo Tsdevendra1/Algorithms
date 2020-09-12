83cd09e54fe6478795797ae23eb9a2e3ad6a4ceb
Tharuka Devendra <tsdevendra1@gmail.com>
2020-09-12 15:47:08 +0100

    added top down version for creating subarrays of an array

--- dynamic_programming/merging_intervals/all_sub_arrays/AllSubArrays.cpp
+++ dynamic_programming/merging_intervals/all_sub_arrays/AllSubArrays.cpp
@@ -8,6 +8,52 @@
 
 using namespace std;
 
+void dfsShrinking(int startOfSubArray, int endOfSubArray, vector<int> &array, vector<vector<int>> &answer,
+                  vector<vector<bool>> &seen) {
+    if (endOfSubArray < startOfSubArray) {
+        return;
+    }
+
+    if (seen[startOfSubArray][endOfSubArray]) {
+        return;
+    }
+
+    seen[startOfSubArray][endOfSubArray] = true;
+    answer.emplace_back(array.begin() + startOfSubArray, array.begin() + endOfSubArray + 1);
+    dfsShrinking(startOfSubArray + 1, endOfSubArray, array, answer, seen);
+    dfsShrinking(startOfSubArray, endOfSubArray - 1, array, answer, seen);
+}
+
+vector<vector<int>> getAllSubArraysTopDownShrinking(vector<int> &array) {
+    vector<vector<int>> answer;
+    vector<vector<bool>> seen(array.size(), vector<bool>(array.size(), false));
+    dfsShrinking(0, array.size() - 1, array, answer, seen);
+    return answer;
+}
+
+
+void dfsExpanding(int startOfSubArray, int endOfSubArray, vector<int> &array, vector<vector<int>> &answer) {
+
+    if (endOfSubArray >= array.size()) {
+        return;
+    }
+
+    if (endOfSubArray < startOfSubArray) {
+        dfsExpanding(0, endOfSubArray + 1, array, answer);
+    } else {
+        answer.emplace_back(array.begin() + startOfSubArray, array.begin() + endOfSubArray + 1);
+        dfsExpanding(startOfSubArray + 1, endOfSubArray, array, answer);
+    }
+
+}
+
+
+vector<vector<int>> getAllSubArraysTopDownExpanding(vector<int> &array) {
+    vector<vector<int>> answer;
+    dfsExpanding(0, 0, array, answer);
+    return answer;
+}
+
 vector<vector<int>> getAllSubArrays(vector<int> &array) {
 
     vector<vector<int>> answer;
@@ -26,5 +72,9 @@ vector<vector<int>> getAllSubArrays(vector<int> &array) {
 void testAllSubArrays() {
     vector<int> test = {6, 2, 4};
     auto answer = getAllSubArrays(test);
+    auto answer2 = getAllSubArraysTopDownShrinking(test);
+    auto answer3 = getAllSubArraysTopDownExpanding(test);
     assert(answer.size() == 6);
+    assert(answer2.size() == 6);
+    assert(answer3.size() == 6);
 }
