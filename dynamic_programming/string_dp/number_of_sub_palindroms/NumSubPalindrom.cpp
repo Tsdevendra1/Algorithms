c335b4dfb811dbc7f2f2468f3f617d9119dc9b0d
Tharuka Devendra <tsdevendra1@gmail.com>
2020-10-06 21:10:59 +0100

    added bottom up version of number of sub palindromes

--- dynamic_programming/string_dp/number_of_sub_palindroms/NumSubPalindrom.cpp
+++ dynamic_programming/string_dp/number_of_sub_palindroms/NumSubPalindrom.cpp
@@ -2,8 +2,11 @@
 // Created by Tharuka Devendra on 01/10/2020.
 //
 
+#include <numeric>
 #include "NumSubPalindrom.h"
 #include "string"
+#include "vector"
+#include "iostream"
 
 using namespace std;
 
@@ -43,7 +46,38 @@ int numberOfSubPalindromesBruteForce(string &stringToTest) {
     return answer;
 }
 
+int numberOfSubPalindromesBottomUp(string &stringToTest) {
+    int n = stringToTest.length();
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int subStringStart = n - 1; subStringStart >= 0; --subStringStart) {
+        for (int subStringEnd = subStringStart; subStringEnd < n; ++subStringEnd) {
+            int space = subStringEnd - subStringStart;
+            char letterOne = stringToTest[subStringEnd];
+            char letterTwo = stringToTest[subStringStart];
+            bool isPalindrome = false;
+            if (space == 0) {
+                isPalindrome = true;
+            } else if (letterOne == letterTwo) {
+                if (space == 1 || dp[subStringStart + 1][subStringEnd - 1] == 1) {
+                    isPalindrome = true;
+                }
+            }
+
+            if (isPalindrome) {
+                dp[subStringStart][subStringEnd] = 1;
+            }
+        }
+    }
+
+    int count = 0;
+    for (auto &array: dp) {
+        count += accumulate(array.begin(), array.end(), 0);
+    }
+    return count;
+}
+
 void testNumSubPalindrom() {
-    string test = "aaab";
-    assert(numberOfSubPalindromesBruteForce(test) == 7);
+    string test = "aaa";
+    assert(numberOfSubPalindromesBottomUp(test) == 6);
+    assert(numberOfSubPalindromesBruteForce(test) == 6);
 }
